ade904ed14f0c44baaca990b6ade84607ee9cd28
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-13 23:29:44 +0500

    coding

--- src/app.js
+++ src/app.js
@@ -2,11 +2,29 @@ const express = require('express');
 const app = express();
 
 
+// Cholo ek note application banai
+app.use(express.json());
 
+const notes = []
 
+app.post('/notes', (req, res) => {
+    notes.push(req.body);
+    res.status(201).json({
+        message: 'Note created successfully',
+        note: req.body,
+    });
+})
 
+app.get('/notes', (req, res) => {
+   
 
+    res.status(200).json({
+        message: 'Notes retrieved successfully',
+        notes: notes,
+    });
 
+    
+});
 
 
 module.exports = app;
