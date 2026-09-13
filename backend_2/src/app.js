0f66c4ec30e3593c67821fe9eb358e0ce61d11c8
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-14 00:12:11 +0500

    coding

--- backend_2/src/app.js
+++ backend_2/src/app.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('this is port 3001')
-})
+//! ek notes application banao jis ma ya ya hona chaiye *//
+//* 1, note create hon 
+//* 2, note delete hon  
+//* 3, note update hon   
+//* 4, Sare notes dekhna chaiye   
+
+app.post()
+
+
+
 
 module.exports = app
