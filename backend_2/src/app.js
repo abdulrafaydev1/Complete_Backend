858b153d98c800f455c51a38840afacdc763d773
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-14 00:19:31 +0500

    coding

--- backend_2/src/app.js
+++ backend_2/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 //! ek notes application banao jis ma ya ya hona chaiye *//
 //* 1, note create hon 
@@ -7,9 +8,21 @@ const app = express()
 //* 3, note update hon   
 //* 4, Sare notes dekhna chaiye   
 
-app.post()
+const notes = []
 
+app.post('/note', (req, res) => {
+    notes.push(req.body)
+    res.status(201).send({
+        message: 'note created suscessfull',
+        note: notes
+    })
+})
 
+app.get('/note', (req, res) => {
+
+    
+    
+})
 
 
 module.exports = app
