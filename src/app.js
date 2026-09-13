04f69e7f7b8ebb5597353d9bf5fd219246279223
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-13 23:56:02 +0500

    coding

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 
 // Cholo ek note application banai
-app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Note Application');
+})
 
 const notes = []
 
+// Create a new note
 app.post('/notes', (req, res) => {
     notes.push(req.body);
     res.status(201).json({
@@ -15,16 +20,32 @@ app.post('/notes', (req, res) => {
     });
 })
 
+// Get all notes
 app.get('/notes', (req, res) => {
-   
 
     res.status(200).json({
         message: 'Notes retrieved successfully',
         notes: notes,
-    });
+    });  
 
-    
 });
+
+// Get a specific note by index and delete it
+app.delete('/notes/:index', (req, res) => {
+
+    const i = req.params.index;
+
+    delete notes[i];
+
+    res.status(200).json({
+        message: 'Note deleted successfully',
+    });
+})
+
+
+
+
+
 
 
 module.exports = app;
