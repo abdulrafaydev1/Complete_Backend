a82522a51294e87864d43c7ee9123163d182aea4
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-14 17:44:04 +0500

    add note model with title and description fields

---
+++ backend_4/src/models/note.model.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const noteSchema = new mongoose.Schema({
+    title: String,
+    description: String
+})
+
+const notemodel = mongoose.model("note", noteSchema)
