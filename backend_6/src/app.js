1bb6702718c1e45deaa7c7e5ba0ea4183942fe94
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-15 08:02:39 +0500

    codng

--- backend_6/src/app.js
+++ backend_6/src/app.js
@@ -7,15 +7,25 @@ app.use(express.json())
 
 app.post('/notes', (req, res) => {
     const data = req.body
-
     noteModel.create({
         title: data.title,
         description: data.description
     })
-
     res.status(201).send({
         message: 'note created'
     })
+})
+
+app.get('/notes', async (req, res) => {
+    const notes = await noteModel.find()
+    res.status(200).send({
+        message: 'notes fatched',
+        notes: notes
+    })
+})
+
+app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id
 })
 
 
