9d87e5ae2f1a309cd37ae49016edf6355482f32a
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-15 12:57:47 +0500

    coding

--- backend_8/src/app.js
+++ backend_8/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const multer = require('multer')
 const uploadFile = require('./services/storage.service')
+const postmodel = require('./models/post.model')
 
 const app = express()
 app.use(express.json())
@@ -12,11 +13,16 @@ app.post('/create-post', upload.single('image'), async (req, res) => {
     console.log(req.file)
 
     const result = await uploadFile(req.file.buffer)
-    console.log(result)
 
-    // res.status(200).send({
-    //     mesage: 'Nice chal raha hai'
-    // })
+    const post = postmodel.create({
+        image: result.url,
+        caption: req.body.caption
+    })
+  
+    return res.status(201).send({
+        mesage: 'post created',
+        post
+    })
     
 })
 
