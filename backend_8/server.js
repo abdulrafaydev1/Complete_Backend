10f5153cf5e8a315ad9bc8520c5a4b461ffff911
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-15 11:05:50 +0500

    coding

--- backend_8/server.js
+++ backend_8/server.js
@@ -1,5 +1,7 @@
 const app = require('./src/app')
+const mongoConnect = require('./src/db/db')
+mongoConnect()
 
 app.listen(3007, () => {
-    console.log()
+    console.log('server is running at port 3007')
 })
---
+++ backend_8/src/db/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const mongoConnect = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://ar2927748_db_user:3nriYgTz83u27DGJ@cluster0.kwosfac.mongodb.net/Complete_backend_5')
+        console.log('mongodb connected')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = mongoConnect
