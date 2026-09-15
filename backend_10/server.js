9decaaa47314829c6878405e5631f3fe24ec96f7
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-16 03:12:45 +0500

    coding

--- backend_10/server.js
+++ backend_10/server.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const app = require('./src/app')
 const mongodbConnect = require('./src/db/db')
 mongodbConnect()
-
 app.listen(3011, () => {
     console.log('server is running on port 3011')
 })
