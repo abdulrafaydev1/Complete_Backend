88e4baa484275999fa797ef4f1952d22a5c1f638
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-15 10:23:27 +0500

    coding

--- backend_7/src/app.js
+++ backend_7/src/app.js
@@ -5,15 +5,17 @@ const noteModel = require('./models/note.model')
 
 app.post('/notes', (req, res) => {
     const data = req.body
-
     noteModel.create({
         title: data.title,
         description: data.description
     })
-
     res.status(201).send({
         messag: 'note created'
     })
+})
+
+app.get('/notes', (req, res) => {
+    
 })
 
 module.exports = app
