61787cea825d981c9735296f5c82fde1c3e4297a
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-18 03:55:57 +0500

    coding

--- backend_17/backend/src/app.js
+++ backend_17/backend/src/app.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const authRoute = require('./routes/auth.routes')
 
 const app = express()
+app.use(express.json())
+
+app.use('/api/auth', authRoute)
 
 module.exports = app
