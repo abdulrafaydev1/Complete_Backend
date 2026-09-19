2dca3b436df32502e74771f416a9c6f8f68d4742
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-19 18:33:11 +0500

    coding

--- backend_21/src/controllers/music.controller.js
+++ backend_21/src/controllers/music.controller.js
@@ -6,12 +6,31 @@ const createMusic = async (req, res) => {
 
     const token = req.cookies.token
 
-    if(!token){
+    if (!token) {
         return res.status(401).json({
             message: 'Unauthorized'
         })
     }
 
-    
-    
+    try {
+
+        const decoded = jwt.verify(token, process.env.JWT_SERECT)
+
+        if (decoded.role !== 'artist') {
+            return res.status(403).json({
+                message: 'Forbideen'
+            })
+        }
+
+    } catch (error) {
+        return res.status(401).json({
+            message: 'Unauthorized token sahi nahi hai'
+        })
+    }
+
+    const { title } = req.body
+    const file = req.file
 }
+
+
+
