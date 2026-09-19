24c3103b8799467284e58193f68685a7e6bfd549
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-19 13:25:30 +0500

    coding

--- backend_20/backend/src/controllers/auth.controller.js
+++ backend_20/backend/src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
-const userModel = require('../models/user.models') 
+const userModel = require('../models/user.models')
+const jwt = require('jsonwebtoken')
 
 const registerUser = async () => {
 
@@ -11,9 +12,17 @@ const registerUser = async () => {
         ]
     })
 
-    if(checkUserExsits){
+    if (checkUserExsits) {
         return res.status(409).json({
             message: 'user already exist'
         })
-    }    
+    }
+
+    const userCreate = userModel.create({
+        username, email, password, role
+    })
+
+    const token = jwt.sign({
+        id: user._id,
+    }, process.env.JWT_SERECT)
 }
