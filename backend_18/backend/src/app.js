e0ff1fdc18592c66d05d311fbca0506667801a16
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-19 05:39:50 +0500

    coding

--- backend_18/backend/src/app.js
+++ backend_18/backend/src/app.js
@@ -4,9 +4,9 @@ const postModel = require('./models/post.model')
 const app = express()
 app.use(express.json())
 
-app.post('/create-post', (req, res) => {
+app.post('/create-post', async (req, res) => {
 
-    const posts = postModel.create({
+    const posts = await postModel.create({
         caption: req.body.caption
     })
 
