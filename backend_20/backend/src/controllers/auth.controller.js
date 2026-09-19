9783da2e89f83a517a1f9c0d73acb7138f045b34
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-19 11:58:51 +0500

    coding

--- backend_20/backend/src/controllers/auth.controller.js
+++ backend_20/backend/src/controllers/auth.controller.js
@@ -2,8 +2,17 @@ const userModel = require('../models/user.models')
 
 const registerUser = async () => {
 
-    const { username, email, password } = req.body
+    const { username, email, password, role = 'user' } = req.body
 
-    
+    const checkUserExsits = userModel.findOne({
+        $or: [
+            { username },
+            { email }
+        ]
+    })
+
+    userModel
+
+
     
 }
