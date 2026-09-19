d886799ee6852c94c5bd0957f33d9216f68ee6e3
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-19 06:57:06 +0500

    coding

---
+++ backend_19/backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+const userModel = require('../models/user.model')
+
+const registerUser = async (req, res) => {
+
+
+    const { username, email, password } = req.body
+    
+}
+
+module.exports = registerUser
+
