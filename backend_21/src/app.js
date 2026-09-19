151119444812720ee02a3510b1fb7e6be23f8d88
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-19 17:59:52 +0500

    coding

--- backend_21/src/app.js
+++ backend_21/src/app.js
@@ -1,12 +1,13 @@
 const express = require('express')
-const authRoutes = require('./routes/auth.routes')
 const cookieParser = require('cookie-parser')
+const authRoutes = require('./routes/auth.routes')
+const musicRoutes = require('./routes/music.routes')
 
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
 
 app.use('/api/auth', authRoutes)
-app.use('/api.auth', authRoutes)
+app.use('/api/auth', musicRoutes)
 
 module.exports = app
