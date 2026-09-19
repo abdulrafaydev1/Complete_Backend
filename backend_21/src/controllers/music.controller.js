5e749426fe8a3b7cff97567b8f7a66f0ab56f9cc
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-19 18:21:12 +0500

    coding

--- backend_21/src/controllers/music.controller.js
+++ backend_21/src/controllers/music.controller.js
@@ -1,9 +1,17 @@
 const musicModel = require('../models/music.model')
+const jwt = require('jsonwebtoken')
 
 const createMusic = async (req, res) => {
 
 
+    const token = req.cookies.token
 
+    if(!token){
+        return res.status(401).json({
+            message: 'Unauthorized'
+        })
+    }
 
+    
     
 }
