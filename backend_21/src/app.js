d409776cd2107a0112269071396c04c743fa7424
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-19 17:51:44 +0500

    coding

--- backend_21/src/app.js
+++ backend_21/src/app.js
@@ -7,5 +7,6 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use('/api/auth', authRoutes)
+app.use('/api.auth', authRoutes)
 
 module.exports = app
--- backend_21/src/controllers/auth.controller.js
+++ backend_21/src/controllers/auth.controller.js
@@ -1,29 +1,75 @@
 const userModel = require('../models/user.model')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
 const registerUser = async (req, res) => {
-
     const { username, email, password, role = 'user' } = req.body
-
-    const checkUserAlreadyExists = userModel.findOne({
+    const checkUserAlreadyExists = await userModel.findOne({
         $or: [
             { username },
             { email },
         ]
     })
-
     if (checkUserAlreadyExists) {
         return res.status(409).json({
             message: 'user already exists'
         })
     }
-
-    const user = userModel.create({
+    const hast = await bcrypt.hash(password, 10)
+    const user = await userModel.create({
         username,
         email,
-        password,
+        password: hast,
         role
     })
-
+    const token = jwt.sign({
+        id: user._id,
+        role: user.role
+    }, process.env.JWT_SERECT)
+    res.cookie('token', token)
+    res.status(201).json({
+        message: 'user created',
+        user
+    })
 }
 
-module.exports = {registerUser}
+const loginUser = async (req, res) => {
+
+    const { username, email, password  } = req.body
+
+    const user = await userModel.findOne({
+        $or: [
+            { username },
+            { email }
+        ]
+    })
+
+    if(!user) {
+        return res.status(401).json({
+            message: 'invalid creedentials rafay'
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if(!isPasswordValid) {
+        return res.status(401).json({
+            message: 'invalid creedentials'
+        })
+    }
+
+    const token = jwt.sign({
+        id: user._id,
+        role: user.role,
+    }, process.env.JWT_SERECT)
+
+    res.cookie('token', token)
+
+    res.status(200).json({
+        message: 'user login',
+        user
+    })
+ 
+}
+
+module.exports = {registerUser, loginUser}
