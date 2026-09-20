36b5c6ad6b6b13ad47b3363520acb1af9acb7386
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-20 20:33:09 +0500

    coding

--- backend_23/src/app.js
+++ backend_23/src/app.js
@@ -1,5 +1,39 @@
 const express = require('express')
 
 const app = express()
+app.use(express.json())
+
+const users = []
+
+
+
+app.post('/users', (req, res) => {
+
+    const { username, email, password } = req.body
+
+    const newUser = {
+        username,
+        email,
+        password
+    }
+
+    users.push(newUser)
+
+res.status(201).json({
+    message: 'user created',
+    newUser
+})
+
+})
+app.get('/fatch-users', (req, res) => {
+
+    res.setHeader('myName', "rafay")
+
+    res.status(200).json({
+        message: 'this is all users',
+        users
+    })
+})
+
 
 module.exports = app
