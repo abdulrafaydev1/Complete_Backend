83c590dbfa25db3a218eea3362ca18b0261de4d5
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-20 22:29:57 +0500

    coding

--- backend_21/src/app.js
+++ backend_21/src/app.js
@@ -6,6 +6,7 @@ const musicRoutes = require('./routes/music.routes')
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
+// app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/auth', authRoutes)
 app.use('/api/auth', musicRoutes)
