c0abd6fd38398d48e0bd1a0c6a7e183c3f7b55ad
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-22 19:13:50 +0500

    coding

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -1,12 +1,6 @@
-let a = "10";
-let b = 10;
-let c = true;
-let d = "true";
-let e = null;
+let value = "0";
 
-console.log(a == b); // true
-console.log(a === b); // false
-console.log(c == d); // true
-console.log(c == 1); // true
-console.log(e == undefined); // true
-console.log(e === undefined); // false
+console.log(Boolean(value)); // true 
+console.log(Number(value)); // Number
+console.log(typeof value); // number
+console.log(typeof Number(value)); 
