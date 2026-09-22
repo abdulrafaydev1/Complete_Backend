9981b471edfa27b2ddea4936859d7844e6f4a108
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-22 17:39:01 +0500

    coding

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -1,1 +1,8 @@
-console.log(12/6);
+const myName = 'rafay';
+const city = 'karachi'
+let myAge = 17
+myAge = 20
+
+console.log(`this is my name ${myName} and this is my city hare i am from ${city} and this is my age ${myAge}`);
+
+
