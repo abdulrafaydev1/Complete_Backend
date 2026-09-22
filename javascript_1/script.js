5484de560a1c1b6740acaafb7315074e57aa099b
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-22 10:37:58 +0500

    coding

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -5,3 +5,10 @@ let studentCity = 'karachi'
 console.log(`My name is ${studentName} and my age is ${studentAge} and my city name is ${studentCity}`);
 
 
+let score = 50;
+score = 80
+
+console.log(score);
+
+ 
+
