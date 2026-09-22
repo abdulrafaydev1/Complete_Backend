a7ddbf0870c2ebb106c4ab7bc6de9a0708a5f34c
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-22 18:28:20 +0500

    coding

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -1,17 +1,32 @@
 var x = 100;
 
 function test() {
-    console.log(x, "this is");
+    // console.log(x, "this is");
 
     var x = 200;
 
     {
         let x = 300;
         console.log(x);
-    }
+
+        {
+            let y = 400;
+            console.log(y);
+
+            {
+                let b = 500;
+                console.log(b)
+
+                {
+                    let ab = 600;
+                    console.log(ab)
+                }
+            }
+        }
+    } 
 
     console.log(x);
 }
 
-console.log(x);
 test();
+console.log(x);
