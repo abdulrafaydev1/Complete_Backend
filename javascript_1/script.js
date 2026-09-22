d4b3c100a1356d199b5165742e8c94d24a811ac7
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-22 19:09:51 +0500

    coding

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -1,4 +1,12 @@
-let value = "100abc";
+let a = "10";
+let b = 10;
+let c = true;
+let d = "true";
+let e = null;
 
-console.log(Number(value));
-console.log(typeof Number(value));
+console.log(a == b); // true
+console.log(a === b); // false
+console.log(c == d); // true
+console.log(c == 1); // true
+console.log(e == undefined); // true
+console.log(e === undefined); // false
