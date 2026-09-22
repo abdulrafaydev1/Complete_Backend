d40adbbfae662eb990656240cd4a0a1f19e53efa
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-22 10:42:44 +0500

    coding

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -10,5 +10,26 @@ score = 80
 
 console.log(score);
 
- 
+var name = 'rafay'
+name = 'ali'
+console.log(name);
 
+
+let product = "Laptop";
+let price = 80000;
+let quantity = 2;
+
+console.log(`this is all price ${price + quantity} my this is your ouder ${product}`);
+
+let marks = 75;
+let totalMarks = 100
+
+console.log(marks + totalMarks);
+
+var name = "Ali";
+var name = "Ahmed";
+console.log(name);
+
+let age = 20;
+age = 25;
+console.log(age);
