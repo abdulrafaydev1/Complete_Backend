644ef66fd445e5dba52ab580215b9900af18900b
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-22 18:46:05 +0500

    coding

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -1,5 +1,3 @@
-console.log(typeof "Hello");
-console.log(typeof 500);
-console.log(typeof true);
-console.log(typeof undefined);
-console.log(typeof null);
+let value = 0;
+
+console.log(Boolean(value));
