c35c814c66b3705ba9e0a4130d4cfeece078c239
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-22 09:37:51 +0500

    coding

--- javascript_1/test.js
+++ javascript_1/test.js
@@ -2,6 +2,11 @@ const accoundId = 10001011
 let accoundEmail = "rafay@gmail.com"
 var accoundPassword = "rafay123"
 
+accoundEmail = 'updatedEmail@gmail.com'
+accoundPassword = '2121212121'
+
+console.table([accoundEmail, accoundPassword]);
+
 
 
 
