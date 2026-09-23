0a4c5f9a951fe34ea055c8ab23e0def77944dad2
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-23 18:02:39 +0500

    coding

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -1,3 +1,6 @@
-let colors = [ 'Green', 'Yello' ]
-colors.splice(1, 1, "red" , "blue")
-console.log(colors);
+let arr =  [ 10, 30, 72, 38, 82, 81, 10, 9 ,8, 16 ]
+const newarr = arr.filter(function(val) {
+    if(val > 10) return val
+})
+
+console.log(newarr);
