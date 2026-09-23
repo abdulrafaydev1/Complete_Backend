6bf006b68b6b82ba7472eef6b5d96098eddafcfd
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-23 19:23:30 +0500

    coding

---
+++ javascript_1/script.js
@@ -0,0 +1,13 @@
+const user = {
+    name: 'rafay',
+    address: {
+        city: 'karachi',
+        pin: 726836926,
+        location: {
+            lat: 23.1,
+            lng: 77.4
+        }
+    }
+}
+
+console.log(user.address.location.lat)
