7aace32eab804fc21cf0d80d0644c67c3086e4dd
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-23 10:05:55 +0500

    coding

--- javascript_1/script.js
+++
@@ -1,9 +0,0 @@
-let a = 0;
-let b = "0";
-let c = null;
-let d = undefined;
-
-console.log(Boolean(a)); // false
-console.log(Boolean(b)); // true
-console.log(Boolean(c)); // false
-console.log(Boolean(d)); // false
