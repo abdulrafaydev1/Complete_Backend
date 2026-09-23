59c32ce4c8c1b2cdb95ef104cc102be422d68ba7
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-23 14:35:30 +0500

    coding

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -1,10 +1,20 @@
 function getScore(...score) {
-    score.forEach(function () {
-        let totle = score
-        totle = totle = score
+    let totle = 0;
+    score.forEach(function (val) {
+        totle = totle + val 
     })
 
+    return totle
 }
 
-let value = getScore(12, 12, 12, 12)
+let value = getScore(12,12,12,12)
 console.log(value)
+
+// function abcd() {
+//     let a = 1
+//     return function () {
+//         console.log(a)
+//     }
+// }
+
+// console.log(abcd()())
