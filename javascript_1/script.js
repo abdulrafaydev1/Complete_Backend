1c55b29e7842ad77c60882c763e6363ca4041414
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-23 12:13:50 +0500

    coding

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -1,1 +1,11 @@
- 
+function number(...val) {
+    let totle = 0;
+    val.forEach(function(fullVal){
+        totle = totle + fullVal
+    })
+
+    console.log(totle);
+    
+}
+
+console.log(number(1,2,3,4,5,6,7,8,9,10))   
