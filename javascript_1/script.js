cb4733e9f5491e6cd05ff948d5cabf84464614d1
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-24 13:02:38 +0500

    coding

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -1,7 +1,24 @@
-let a = { value: 10 };
-let b = a;
+/* 
+   console.log(typeof null);            -> object
+   console.log(typeof NaN);             -> number
+   console.log(typeof Infinity);        -> number
+   console.log(typeof []);              -> object
+   console.log(typeof {});              -> object
+   console.log(typeof function () {});  -> function  
 
-b.value = 50;
+   console.log("10" == 10);    -> true
+   console.log("10" === 10);   -> false
+   console.log(true == 1);     -> true
+   console.log(true === 1);    -> false
+   console.log(false == 0);    -> true
+   console.log(false === 0);   -> false
 
-console.log(a.value);
-console.log(b.value);
+   console.log([] == false);   -> true
+   console.log([] === false);  -> error
+   console.log("" == false);   -> true
+   console.log("" === false);  -> false
+*/
+
+
+
+
