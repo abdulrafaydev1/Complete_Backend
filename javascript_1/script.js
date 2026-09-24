0b87d3185b33d9b9ab420da6b29fb94d34cc426f
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-24 10:15:49 +0500

    coding

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -1,25 +1,27 @@
-let x = 10;
+// let x = 10;
 
-function outer() {
-    console.log(x);
+// function outer() {
+//     // console.log("is console ma ayeega undefiend", x); // is console ma ayeega undefiend
 
-    let x = 20;
+//     let x = 20;
 
-    function inner() {
-        console.log(x);
+//     function inner() {
+//         // console.log("this console ayega (20)", x); // is console ma ayeega ( 20 )
 
-        let x = 30;
+//         let x = 30;
 
-        console.log(x);
-    }
+//         console.log("is console ma ayega 30", x); // is console ma ayega 30
+//     }
 
-    inner();
+//     inner();
 
-    console.log(x);
+//     console.log("is console ma ayeega 20", x); // or is console ma ayeega ( 20 )
+// }
+
+// outer();
+
+// console.log("is console ma ayega 10", x); // is console ma ayeega ( 10 )
+
+function abcd(){
+    
 }
-
-outer();
-
-console.log(x); // 
-
- 
