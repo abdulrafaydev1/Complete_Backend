728d04c8e59a90a9b9d160df740147ca8d40eec2
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-24 17:40:31 +0500

    coding

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -270,6 +270,9 @@ Ans, sir is code ma error ayeega
 Q38, var declaration hoisting aur initialization mein kya difference hai?
 ans, sir var ma hoisting hote hai or var variable declaration part uper chala jata hai or initialization part niche rah jata hai or ager haam variable ko koi value nahi data to javascript us variable ma undifiend value dall data hai isi liya var keyword se bana wuwa variable hoist hai likin koi error nahi data undifiend ata hai
 
+Q39, let aur const ko hoisted kehne ke bawajood unko declaration se pehle access kyun nahi kar sakte?
+Ans, let aur const hoist hote hain, lekin unki declaration se pehle unka variable Temporal Dead Zone (TDZ) mein hota hai.
+
 
 */
 
