f8e325bf374a25db6aecbf3fb5aef2f92ae9a806
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-24 17:41:03 +0500

    coding

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -273,6 +273,9 @@ ans, sir var ma hoisting hote hai or var variable declaration part uper chala ja
 Q39, let aur const ko hoisted kehne ke bawajood unko declaration se pehle access kyun nahi kar sakte?
 Ans, let aur const hoist hote hain, lekin unki declaration se pehle unka variable Temporal Dead Zone (TDZ) mein hota hai.
 
+Q40, Temporal Dead Zone kya hoti hai?
+Ans, is ka pata nahi
+
 
 */
 
