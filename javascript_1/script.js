cc7ae11f8005d3c2b5822614fa52cea44466aeab
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-24 13:22:48 +0500

    coding

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -17,8 +17,37 @@
    console.log([] === false);  -> error
    console.log("" == false);   -> true
    console.log("" === false);  -> false
+
+   let a = "5";
+   let b = 2;
+
+   console.log(a + b);         -> 52
+   console.log(a - b);         -> 3
+   console.log(a * b);         -> 10
+   console.log(a / b);         -> 2.5
+   console.log(a % b);         -> 1
+
+   let x = "10";
+   console.log(+x); 
+   console.log(typeof +x); 
+   console.log(+"hello"); 
+   console.log(typeof +"hello");
 */
 
+
+let a = "10";
+let b = 5;
+let c = true;
+let d = null;
+let e = undefined;
+
+console.log(a + b); // 555
+console.log(a - b); // 5
+console.log(b+ c); // 6
+console.log(b - c); // 4
+console.log(d + b); // 5
+console.log(e + b); // 5
+ 
 
 
 
