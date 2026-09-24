a94b8c3fe23857f199a36aeac9b78f0d4d1c72c0
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-24 09:06:44 +0500

    coding

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -1,42 +1,13 @@
-// let x = 100;
-// function outer() {
-//     let x = 200;
+let x = 100;
 
-//     function inner() {
-//         let x = 300;
-//         console.log(x);
-//     }
+function outer() {
+    let x = 200;
 
-//     inner();
-//     console.log(x);
-// }
+    function inner() {
+        console.log(x);
+    }
 
-// outer();
-// console.log(x);
-
-
-// let a = 10;
-
-// function test() {
-//     let a = 20;
-
-//     if (true) {
-//         let a = 30;
-//         console.log(a);
-//     }
-
-//     console.log(a);
-// }
-
-// test();
-// console.log(a);
-
-// // is function ka output hoga 30 20 10
-
-const a = 20;
-function abcd() {
-    a++
-    console.log(a);
+    inner();
 }
 
-abcd()
+outer();
