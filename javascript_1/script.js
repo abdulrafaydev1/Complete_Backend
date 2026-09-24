2249c4d56885f9a40f543883e58b466c02315c37
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-24 16:58:41 +0500

    coding

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -161,6 +161,10 @@ Q12, var x = 10;
      console.log(x);
 Ans, sir is code ma ya ho raha hai ky ( var ) keyword se ek variable bana hai or or ma us variable ma value to reassign kar raha hai
 
+Q13, let x = 10;
+     let x = 20;
+Ans, sir is keyword ma yaa ho raha hai ky ( let ) keyword se bana variable ko redeclare kar raha hai to is code ma error ayega ( let ) keyword se bana huwa variable redeclare nahi ho sata
+
 
 */
 
