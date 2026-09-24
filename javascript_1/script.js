08b5c936523b7e712908137c4da97d398c88add4
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-24 12:09:39 +0500

    coding

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -1,12 +1,12 @@
 var x = 1;
 
 function level1() {
-    console.log(x);
+    // console.log(x); //* is console per ayega error
 
     let x = 2;
 
     function level2() {
-        console.log(x);
+        console.log("is console ma ayegaa undifiend", x); //* is console ma ayegaa undifiend
 
         var x = 3;
 
@@ -16,25 +16,25 @@ function level1() {
             console.log(x);
 
             function level3() {
-                console.log(x);
+                // console.log(x); // error
 
                 let x = 5;
 
-                console.log(x);
+                console.log("is console ma ayega 5", x); //* is console ma ayega 5
             }
 
             level3();
 
-            console.log(x);
+            console.log("is console ma ayega 4", x); //* is console ma ayega 4
         }
 
-        console.log(x);
+        console.log("is console ma ayega 3",x); //* is console ma ayega 3
     }
 
     level2();
 
-    console.log(x);
+    console.log("is console ma ayega ( 2 )", x); //* is console ma ayega ( 2 )
 }
 
 level1();
-console.log(x);
+console.log("is console ma ayega ( 1 ) ", x); //* is console ma ayegaa 1
