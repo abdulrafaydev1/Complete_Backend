919f2d81802270a5e0396ff4e19797d6ad85c11a
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-25 18:06:02 +0500

    coding

--- javascript_DOM/JSDOM.js
+++ javascript_DOM/JSDOM.js
@@ -1,6 +1,2 @@
-let a = document.querySelector('a')
-a.setAttribute('href', "https://www.youtube.com")
-a.setAttribute('id', 'rafay')
-const an = document.getElementById('rafay')
-an.style.color = 'red'
-console.log(a); 
+const img = document.querySelector('img')
+img.setAttribute('src', '')
