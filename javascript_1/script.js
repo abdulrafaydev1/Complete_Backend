ee34283fb7169b3e5161bcbd2b6085ba5f75d3b9
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-25 13:40:43 +0500

    coding

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -292,7 +292,7 @@ if (name) {
 for (let i = 1; i <= 5; i++){
      console.log('this is product id', i);
 }
-
-for (let i = 5; i >= 1; i--) {
-    console.log(i);
+for(let i = 5; i >= 1; i--){
+     console.log(i);
+     
 }
