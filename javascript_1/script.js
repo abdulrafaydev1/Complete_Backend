8b66a3c9c8991e54a845f11f0a5b9e0e0ddcbe0d
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-25 15:28:16 +0500

    coding

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -325,44 +325,37 @@ for (let j = 1; j <= 3; j++) {
 
 
 //       0      0
-for (let i = 0; i < 10; i += 2) {
+// for (let i = 0; i < 10; i += 2) {
 
-    for (let j = 0; j < 5; j += 2) {
-        console.log(i, j);
-    }
+//     for (let j = 0; j < 5; j += 2) {
+//         console.log(i, j);
+//     }
+// }
+
+//              8 
+for (let i = 0; i < 10; i += 2) {
+ 
+     for(let j = 0; i < 3; i += 1){
+          console.log(`this is i value ${i} and this is j value ${j}`)
+     }
 }
 /*
-0 1
-0 2
-0 3
-0 4
-0 5
-2 1  
-2 2
-2 3
-2 4
-2 5
-4 1
-4 2
-4 3
-4 4
-4 5
-6 1
-6 2
-6 3
-6 4
-6 5
-8 1
-8 2
-8 3
-8 4
-8 5
-
-
+0
+0
+0
+2
+2
+2
+4
+4
+4
+6
+6
+6
+8
+8
+8
 */
-for (let i = 0; i <= 5; i += 2) {
-     console.log(i);
-}    
 
 // for (let i = 1; i <= 3; i++) {
 
