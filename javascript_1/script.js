028883080c621d1dd75550a1d5865e8add433e22
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-25 14:29:36 +0500

    coding

--- javascript_1/script.js
+++ javascript_1/script.js
@@ -292,7 +292,7 @@ let arr = [ 'apple', 'banana', 'kivi' ];
 for(let i = 0; i < arr.length; i++){
      console.log(arr[i]);
      
-}
+} 
 
 for (let i = 0; i < 5; i++) {
     if (i === 2) {
@@ -301,11 +301,19 @@ for (let i = 0; i < 5; i++) {
 
     console.log(i);
 }
-*/
-for (let i = 1; i <= 10; i++) {
-    if (i === 6) {
-        break;
-    }
 
-    console.log(i);
+for (let i = 1; i <= 3; i++) {
+
+     for (let j = 1; j <= 3; j++) {
+          console.log(i, j);
+     }
+}
+*/
+
+for(let i = 0; i >= 5; i++){
+
+     for(let j = 0; j >= 3; i++){
+          console.log(i,j);
+     }
+     
 }
