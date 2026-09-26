625f533baec2051acfabd9bf4602e1416124a68c
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-26 21:52:57 +0500

    coding

--- javascript_DOM/JSDOM.js
+++ javascript_DOM/JSDOM.js
@@ -2,8 +2,13 @@ let input = document.querySelector('input')
 let h1 = document.querySelector('h1')
 let span = document.querySelector('span')
 
-input.addEventListener('input', function(dets) {
-    
-    span.textContent = dets.target.value
-    
+input.addEventListener('input', function (dets) {
+    let CharactersLeft = 20 - input.value.length
+    span.textContent = CharactersLeft
+
+    if (CharactersLeft < 0) {
+        span.style.color = 'red'
+    } else {
+        span.textContent = left
+    }
 })
