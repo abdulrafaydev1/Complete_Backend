45af3ef1fe148010b5c1fee8eaa4512af3e8fc97
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-26 15:23:37 +0500

    coding

--- javascript_DOM/JSDOM.js
+++ javascript_DOM/JSDOM.js
@@ -2,6 +2,7 @@ let sel = document.querySelector('select')
 let h1 = document.querySelector('h1')
 
 sel.addEventListener('change', function(dets){
+    // h1.textContent = 'device selected'
+    h1.textContent = dets.target.value
     console.log(dets.target.value);
-    
 })
