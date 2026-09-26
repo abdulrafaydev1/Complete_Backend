8417ff4dff660ddf46bb20d2c6cd8ea3e34eb14e
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-26 16:39:43 +0500

    coding

--- javascript_DOM/JSDOM.js
+++ javascript_DOM/JSDOM.js
@@ -1,11 +1,1 @@
-let btn = document.querySelector('#btn')
-let fileInput = document.querySelector('#fileInput')
-
-btn.addEventListener("click", function () {
-    fileInput.click()
-})
-
-fileInput.addEventListener('change', function (dets) {
-    btn.textContent = dets.target.files[0].name
-    console.log(dets.target.files[0].name)
-})
+ 
