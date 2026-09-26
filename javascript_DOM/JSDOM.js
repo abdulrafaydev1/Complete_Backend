8789b145e5d5dc8a808a818583736243e20f6955
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-26 12:41:34 +0500

    coding

--- javascript_DOM/JSDOM.js
+++ javascript_DOM/JSDOM.js
@@ -1,9 +1,4 @@
-let h1 = document.querySelector('h1')
-
-function dblclick() {
-    h1.style.color = 'black'
-    h1.style.backgroundColor = 'white'
-}
-
-h1.addEventListener('dblclick', dblclick)
-h1.removeEventListener('dblclick', dblclick)
+let input = document.querySelector('input')
+input.addEventListener('input', function (e) {
+    console.log('you typed', e.target.text);
+})
