0802d2663cb77c9e4749ef4b3d92657025b040ea
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-26 17:07:59 +0500

    coding

--- javascript_DOM/JSDOM.js
+++ javascript_DOM/JSDOM.js
@@ -1,5 +1,8 @@
 let form = document.querySelector('form')
+let inputs = document.querySelectorAll('input')
 
-form.addEventListener('submit', function(dets){
+form.addEventListener('submit', function (dets) {
+    dets.preventDefault()
 
+    console.log()
 })
