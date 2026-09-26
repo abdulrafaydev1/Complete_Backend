8fe912fb75b6e4b949e69302b30d09387fc2803e
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-26 23:32:14 +0500

    coding

--- javascript_DOM/JSDOM.js
+++ javascript_DOM/JSDOM.js
@@ -1,7 +1,8 @@
 let form = document.querySelector('#todoForm')
 let todoInput = document.querySelector('#todoInput')
-console.log(todoInput)
 let container = document.querySelector('.container')
+let priority = document.querySelector('#priority')
+console.log(priority)
 
 form.addEventListener('submit', function(dets){
     dets.preventDefault()
@@ -11,9 +12,15 @@ form.addEventListener('submit', function(dets){
 
     let h3 = document.createElement('h3')
     h3.textContent = todoInput.value
-    
 
+    let span = document.createElement('span')
+    span.classList.add('select')
+    span.textContent = priority.value
+    
+    h3.appendChild(span)
     todoCard.appendChild(h3)
 
     container.appendChild(todoCard)
+
+    console.log(todoCard)
 })
