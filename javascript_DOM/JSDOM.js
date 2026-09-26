163ed354d0e7f884b2e60b532b2cc8212882dc98
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-26 19:34:58 +0500

    coding

--- javascript_DOM/JSDOM.js
+++ javascript_DOM/JSDOM.js
@@ -1,1 +1,7 @@
- 
+ let abcd = document.querySelector('#abcd')
+ abcd.addEventListener('mouseover', function(){
+    abcd.style.backgroundColor = 'green'
+ }) 
+ abcd.addEventListener('mouseout', function(){
+    abcd.style.backgroundColor = 'red'
+ }) 
