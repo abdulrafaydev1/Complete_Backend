c7838a2c9fbbcafe205a0713920f2847a04c7a2e
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-26 17:18:31 +0500

    coding

--- javascript_DOM/JSDOM.js
+++ javascript_DOM/JSDOM.js
@@ -4,10 +4,8 @@ let inputs = document.querySelectorAll('input')
 form.addEventListener('submit', function (dets) {
     dets.preventDefault()
 
-    console.log(
-        inputs[0].value,
-        inputs[1].value,
-        inputs[2].value,
-        inputs[3].value
-    )
+    inputs.forEach(function(){
+        console.log(dets.submitter.value)
+    })
+   
 })
