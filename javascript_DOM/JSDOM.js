e9a79e27a6630c73aaef99040084e2991e09d863
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-26 23:57:39 +0500

    coding

--- javascript_DOM/JSDOM.js
+++ javascript_DOM/JSDOM.js
@@ -2,9 +2,10 @@ let form = document.querySelector('#todoForm')
 let todoInput = document.querySelector('#todoInput')
 let container = document.querySelector('.container')
 let priority = document.querySelector('#priority')
+
 console.log(priority)
 
-form.addEventListener('submit', function(dets){
+form.addEventListener('submit', function (dets) {
     dets.preventDefault()
 
     let todoCard = document.createElement('div')
@@ -12,6 +13,14 @@ form.addEventListener('submit', function(dets){
 
     let h3 = document.createElement('h3')
     h3.textContent = todoInput.value
+    h3.addEventListener('click', () => {
+        h3.classList.toggle('lt')
+        if (h3.style.color === 'green') {
+            h3.style.color = 'white'
+        } else {
+            h3.style.color = 'green'
+        }
+    })
 
     let span = document.createElement('span')
     span.classList.add('select')
@@ -20,7 +29,10 @@ form.addEventListener('submit', function(dets){
     let btn = document.createElement('button')
     btn.classList.add('btn')
     btn.textContent = 'Delete ToDo'
-    
+    btn.addEventListener('click', () => {
+        todoCard.remove()
+    })
+
     h3.appendChild(span)
     todoCard.appendChild(h3)
     todoCard.appendChild(btn)
