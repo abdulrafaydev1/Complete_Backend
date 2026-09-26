3bcae80043b06dc327666806d4c1115b512a9f5d
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-26 18:10:40 +0500

    coding

--- javascript_DOM/JSDOM.js
+++ javascript_DOM/JSDOM.js
@@ -10,7 +10,21 @@ form.addEventListener('submit', function (dets) {
     let profile = document.createElement("div")
     profile.classList.add("profile")
 
+    let img = document.createElement("img")
+    img.setAttribute('src', 'https://static.vecteezy.com/system/resources/thumbnails/083/933/835/small/beautiful-and-inspiring-picture-detailing-a-bright-hot-air-balloon-over-river-pure-cozy-perfect-for-creatives-moods-stock-image-free-photo.jpeg')
+
+    let h3 = document.createElement('h3')
+    h3.textContent = 'Abdul Rafay'
+
+    let h5 = document.createElement('h5')
+    h5.textContent = 'Creative Coder'
+
+    let p = document.createElement('p')
+    p.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit   Provident natus beatae tempora iure, dignissimos eius numquam fuga'
+
+    profile.appendChild(img)
     card.appendChild(profile)
+
     console.log(card)
 
 })
